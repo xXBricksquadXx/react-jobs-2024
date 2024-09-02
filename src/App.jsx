832fa13fb586bfc1bf1a249a55b8841e92767018
Colby Cardell <77302221+xXBricksquadXx@users.noreply.github.com>
2024-09-02 19:27:52 -0400

    JSX Crash Course :wrench:

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  const name = 'John';
+  const x = 10;
+  const y = 20;
+  const names = ['Brad', 'Mary', 'Joe', 'Sarah'];
+  const loggedIn = true;
+
+  return (
+    <>
+      <div className="text-5xl">App</div>
+      <p>Hello {name}</p>
+      <p>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn ? <h1>Hello Member</h1> : <h1>Hello Guest</h1>}
+    </>
+  );
 };
 
 export default App;
