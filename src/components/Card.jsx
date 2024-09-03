71ba9ac61818b6f7735d66131ec6cf4a31d17b5b
Colby Cardell <77302221+xXBricksquadXx@users.noreply.github.com>
2024-09-02 21:35:03 -0400

    cards :wrench:

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
-const Card = ({ children }) => {
-  return <div className="bg-gray-100 p-6 rounded-lg shadow-md">{children}</div>;
+const Card = ({ children, bg = 'bg-gray-100' }) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
 export default Card;
--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -18,7 +18,7 @@ const HomeCards = () => {
             </a>
           </Card>
 
-          <Card>
+          <Card bg="bg-indigo-100">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
               List your job to find the perfect developer for the role
