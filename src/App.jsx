566b9c5a2f5fef721d28567cdd8172fb5ebfd1cc
Colby Cardell <77302221+xXBricksquadXx@users.noreply.github.com>
2024-09-03 18:56:43 -0400

    JobsPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import {
 } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
     </Route>
   )
 );
