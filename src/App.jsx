a0202f651105548822aa8bfe85bbfa345d9268d5
Colby Cardell <77302221+xXBricksquadXx@users.noreply.github.com>
2024-09-03 18:50:08 -0400

    setting up Page "layout" :wrench:

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
 );
 
 const App = () => {
